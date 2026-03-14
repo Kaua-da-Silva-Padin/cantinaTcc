c9f2cddff0627a6da4620a5fc3fa8501e4746d74
Kaua-da-Silva-Padin <kauapadin5@gmail.com>
2026-03-14 13:19:37 -0300

    Testing vercel analytics

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,6 +16,7 @@ export default function Home() {
 
   return(
     <>
+      
       {/*Cabeçalho da págino com profile, username e carrinho de compras*/}
       <Header
       cartPrice={cartPrice}/>
