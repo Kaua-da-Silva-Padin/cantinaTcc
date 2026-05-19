c0ec0b3966c07620d5c6c8e83a71851b16f848c5
Kaua-da-Silva-Padin <kauapadin5@gmail.com>
2026-05-19 18:03:00 -0300

    Fix crossOrigin attribute in preconnect link for fonts.gstatic.com

--- app/root.jsx
+++ app/root.jsx
@@ -12,7 +12,7 @@ export function Layout({ children }) {
         <link rel="icon" href="/imgs/pelicano.jpg" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
         <title>CantinaTec</title>
         <Meta />
