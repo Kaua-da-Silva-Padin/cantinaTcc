24a5c8b95b9aa4121f425aeb129b52a37e4bba87
PauloV-142 <paulo.almeida142@etec.sp.gov.br>
2026-06-04 20:08:55 -0300

    Added static OrderInfo table and finish button.

--- app/Components/OrdersList/OrdersList.jsx
+++ app/Components/OrdersList/OrdersList.jsx
@@ -9,42 +9,6 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
-function SingleOrder({ id, entrance, customerId, productsLength, price }) {
-    
-    const style = {
-        background: "#ffffff",
-        color: "#000000",
-        fontWeight: "bold",
-        fontSize: "1.5em",
-        borderRadius: ".4em",
-        justifyContent: "space-evenly",
-    };
-    
-    const divider = (
-        <Divider 
-        orientation='vertical' 
-        sx={{border: "1px solid darkgray"}} 
-        flexItem />
-    );
-    
-    // Will be replaced by Table
-    return (
-        <Stack
-        className='py-2'
-        direction="row"
-        divider={divider}
-        spacing={2}
-        sx={style}
-        >
-        <span># {id}</span>
-        <span>{entrance}</span>
-        <span>{customerId}</span>
-        <span>{productsLength}</span>
-        <span>R$ {price}</span>
-        </Stack>
-    );
-}
-
 export default function OrdersList({ orders }) {
     const divider = (
         <Divider 
@@ -57,14 +21,20 @@ export default function OrdersList({ orders }) {
         <TableContainer 
         className="bg-dark rounded-2 p-2">
         <Table>
-
             <TableHead> 
+
                 <TableRow className='bg-white' key="24">
-                    {['ID', 'Entrada', 'Cliente', 'Itens', 'Valor']
-                    .map(txt => 
-                    <TableCell className='fw-bold fs-5' align='center'>{txt}</TableCell>)}
+                    
+                    {['ID', 'Entrada', 'Cliente', 'Itens', 'Valor'].map(txt => 
+                        <TableCell 
+                        className='fw-bold fs-5' 
+                        align='center'>
+                            {txt}
+                        </TableCell>
+                    )}
                 </TableRow>
             </TableHead>
+            
             <TableBody>
                 {orders.map((order) => 
                     <TableRow 
@@ -78,7 +48,7 @@ export default function OrdersList({ orders }) {
                         <TableCell align='center' className='fw-bold fs-6'>#{order.id}</TableCell>
                         <TableCell align='center' className='fw-bold fs-6'>{order.entrance}</TableCell>
                         <TableCell align='center' className='fw-bold fs-6'>{order.customer.id}</TableCell>
-                        <TableCell align='center' className='fw-bold fs-6'>{order.products.length}</TableCell>
+                        <TableCell align='center' className='fw-bold fs-6'>{order.items.length}</TableCell>
                         <TableCell align='center' className='fw-bold fs-6'>R$ {order.price}</TableCell>
                     </TableRow>
                 )}
