189559c06d05746d2c5643c035cf2d35f8f266ed
Kaua-da-Silva-Padin <kauapadin5@gmail.com>
2026-07-21 17:32:44 -0300

    Update WeeklySales component to include mobile detection and modify iframe source for dynamic embedding

--- app/Components/WeeklySales/WeeklySales.jsx
+++ app/Components/WeeklySales/WeeklySales.jsx
@@ -1,11 +1,14 @@
+import { useMediaQuery } from '@mui/material';
 
 export default function WeeklySales() {
+
+    const isMobile = useMediaQuery('(max-width: 768px)') ? 'true' : 'false';
 
     return(
         <div
         className="d-flex justify-content-center align-items-center">
             <iframe
-            src="https://os-programadores.appsmith.com/app/lucros-cantinatec/page1-6a580675980d29f395ca698f/embed"
+            src={`https://os-programadores.appsmith.com/app/lucros-cantinatec/page1-6a580675980d29f395ca698f?embed=true&isMobile=${isMobile}`}
             style={{
                 width: '100dvw',
                 height: '83dvh'
